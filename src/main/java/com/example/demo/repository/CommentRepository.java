5f60a32f0fef70a3a33ec071603e7166f5932eb3
zxcvne <nhs8421@gmail.com>
2025-12-23 17:43:20 +0900

    update

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -3,6 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // JpaRepository<Entity, Id Class>
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    // 기본키 (ID)가 아닌 일반 칼럼은 등록을 해야 사용할 수 있음.
+    List<Comment> findByBno(long bno);
 }
--- src/main/java/com/example/demo/service/CommentServiceImpl.java
+++ src/main/java/com/example/demo/service/CommentServiceImpl.java
@@ -1,13 +1,47 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.CommentDTO;
+import com.example.demo.entity.Board;
+import com.example.demo.entity.Comment;
 import com.example.demo.repository.CommentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class CommentServiceImpl implements CommentService{
     private final CommentRepository commentRepository;
+
+    @Override
+    public long post(CommentDTO commentDTO) {
+        // 저장 대상은 Entity CommentDTO => Entity로 변환
+        // save()
+        return commentRepository.save(convertDtoToEntity(commentDTO)).getCno();
+    }
+
+    @Override
+    public List<CommentDTO> getList(Long bno) {
+        /* findBy ** => ** 테이블 안에 있는 모든 칼럼
+        *   select * from comment where ** = ? */
+        List<Comment> list = commentRepository.findByBno(bno);
+
+        return list.stream()
+                .map(comment -> convertEntityToDto(comment))
+                .toList();
+    }
+
+    @Transactional
+    @Override
+    public long modify(CommentDTO commentDTO) {
+        Comment comment = commentRepository.findById(commentDTO.getCno())
+                .orElseThrow(() -> new EntityNotFoundException("해당 댓글을 찾을 수 없습니다."));
+        comment.setContent(commentDTO.getContent());
+        return comment.getCno();
+    }
 }
